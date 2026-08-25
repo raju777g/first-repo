9eb4f3a4d7a542ecdcd860b0051801a7d60bd701
raju777g <rajugarain49@gmail.com>
2026-08-25 17:05:23 +0530

    stable version committed

--- SecondCode.java
+++ SecondCode.java
@@ -2,5 +2,6 @@ class Main{
     public static void main(String[] args){
         System.out.println("Hello world!!!");
         System.out.println("Hello Raj!");
+        System.out.println("First release completed!");
     }
 }
